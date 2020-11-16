46c71da37cc642c1103fcc7973800bbdfd31e2d1
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-11-16 15:11:53 +0300

    created Sign In page

---
+++ models/User.model.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+ 
+const userSchema = new Schema(
+  {
+    username: String,
+    passwordHash: String
+  },
+  {
+    timestamps: true
+  }
+);
+ 
+module.exports = model('User', userSchema);
